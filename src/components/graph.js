f67319ae12e6fa375c2282a1e959a83144838da5
Isaac Ezer <isaac.ezer@gmail.com>
2019-11-04 14:11:51 +0100

    mirage js

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import XYPlot from 'reactochart/XYPlot';
 import XAxis from 'reactochart/XAxis';
 import YAxis from 'reactochart/YAxis';
@@ -6,12 +6,13 @@ import LineChart from 'reactochart/LineChart';
 import 'reactochart/styles.css';
 
 function QuickStartGraph(props) {
-  let data = [
-    {x: 0, y: 20},
-    {x: 5, y: 30},
-    {x: 10, y: 35},
-    {x: 15, y: 30},
-  ];
+  let [ data, setData ] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/datapoints")
+    .then(response => response.json())
+    .then(json => setData(json.datapoints));
+  }, []);
 
   let xTicks = data.map(d => d.x);
   let yTicks = data.map(d => d.y).concat([0, 40]);
---
+++ src/mirage/server.js
@@ -0,0 +1,22 @@
+import { Server } from "miragejs";
+
+export default function makeServer() {
+  let server = new Server({
+    routes() {
+      this.namespace = "api";
+      this.get("/datapoints", () => {
+        console.log('mirage /datapoints!');
+        return {
+          datapoints: [
+            {x: 0, y: 20},
+            {x: 5, y: 30},
+            {x: 10, y: 35},
+            {x: 15, y: 30},
+          ]
+        };
+      });
+    }
+  });
+
+  return server;
+}
