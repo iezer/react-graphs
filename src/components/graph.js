c8bedb95b271b5edf2b0c374816e26a27e333c0e
Isaac Ezer <isaac.ezer@gmail.com>
2019-11-04 14:00:32 +0100

    customize graph ticks

--- src/components/graph.js
+++ src/components/graph.js
@@ -5,18 +5,23 @@ import YAxis from 'reactochart/YAxis';
 import LineChart from 'reactochart/LineChart';
 import 'reactochart/styles.css';
 
-function QuickStartGraph1(props) {
+function QuickStartGraph(props) {
+  let data = [
+    {x: 0, y: 20},
+    {x: 5, y: 30},
+    {x: 10, y: 35},
+    {x: 15, y: 30},
+  ];
+
+  let xTicks = data.map(d => d.x);
+  let yTicks = data.map(d => d.y).concat([0, 40]);
+
   return (
-    <XYPlot width={400} height={300}>
-      <XAxis />
-      <YAxis />
+    <XYPlot width={400} height={300} yDomain={[0, 35]}>
+      <XAxis ticks={xTicks} />
+      <YAxis ticks={yTicks} />
       <LineChart
-        data={[
-          {x: 0, y: 20},
-          {x: 5, y: 30},
-          {x: 10, y: 35},
-          {x: 15, y: 30},
-        ]}
+        data={data}
         x={d => d.x}
         y={d => d.y}
       />
@@ -24,4 +29,4 @@ function QuickStartGraph1(props) {
   );
 }
 
-export default QuickStartGraph1;
+export default QuickStartGraph;
