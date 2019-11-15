d5f3a21b3afb1c6b5d46834aae83969b415d2cb1
Isaac Ezer <isaac.ezer@gmail.com>
2019-11-15 14:13:13 +0100

    wip use force for labels

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -3,6 +3,11 @@ import { geoMercator, geoPath } from "d3-geo";
 import * as d3 from "d3";
 import { feature } from "topojson-client";
 
+// examples of force-directed labels
+// http://bl.ocks.org/ZJONSSON/1691430
+// http://bl.ocks.org/susielu/9526340
+// https://bl.ocks.org/rsk2327/23622500eb512b5de90f6a916c836a40
+// http://bl.ocks.org/MoritzStefaner/1377729 and https://bl.ocks.org/mapio/53fed7d84cd1812d6a6639ed7aa83868
 const MAX_RADIUS = 10;
 
 // from https://medium.com/@zimrick/how-to-create-pure-react-svg-maps-with-topojson-and-d3-geo-e4a6b6848a98
@@ -51,7 +56,7 @@ class WorldMap extends Component {
     console.log(text.join(', '));
   }
 
-  render() {
+  renderNodes() {
     let eventCounts = this.props.markers.map(m => m.events.length);
     let maxEvents = Math.max.apply(null, eventCounts);
 
@@ -59,27 +64,91 @@ class WorldMap extends Component {
       return Math.sqrt(value / maxEvents) * MAX_RADIUS;
     }
 
+    let projection = this.projection();
+    let nodes = this.props.markers.map(function(d) {
+      let { lat, lng, city } = d.location;
+      let [x, y] = projection([lng, lat]);
+      let r = calculateRadius(d.events.length);
+      return {x, y, city, r};
+    });
+
+    let self = this;
+    let g = d3.select('.markers');
+    g.selectAll('circle')
+    .data(nodes)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', function(d) { return d.r; })
+    .attr('fill', "#E91E63")
+    .attr('class', function(d) {
+      return `marker marker-${d.city}`;
+    })
+    .on('click', function(d,i) {
+      self.handleMarkerClick(i);
+    });
+  }
+
+  renderLabels() {
+    let { width } = this.state;
+    // render labels with D3
+    let g = d3.select('.labels');
+    let projection = this.projection();
+
+    let foci = [];
+    let labels = [];
+
+    this.props.markers.forEach(m => {
+      let { lat, lng } = m.location;
+      let [x, y] = projection([lng, lat]);
+      let label = m.location.city.split(',')[0];
+
+      foci.push({x, y});;
+      labels.push({x, y, label});
+    });
+
+    let node = g.selectAll('text')
+        .data(labels)
+        .enter()
+        .append('text')
+        .attr('x', function(d) { return d.x; })
+        .attr('y', function(d) { return d.y; })
+        .attr('text-anchor', 'middle')
+        .text(function(d) { return d.label; })
+        .attr('class', function(d) { return `label label-${d.label}`; });
+
+    // http://bl.ocks.org/pnavarrc/5913636
+    let simulation = d3.forceSimulation(labels)
+        .alphaDecay(0.03)
+        .force('repel', d3.forceManyBody().strength(-120).distance(width / 6))
+        .force('attract', d3.forceManyBody().strength(40).distanceMax(width / 6));
+
+    simulation
+    .on("tick", function() {
+      var k = .1 * simulation.alpha();
+      labels.forEach(function(o, j) {
+        // The change in the position is proportional to the distance
+        // between the label and the corresponding place (foci)
+        o.y += (foci[j].y - o.y) * k;
+        o.x += (foci[j].x - o.x) * k;
+      });
+
+      // Update the position of the text element
+      node
+      .attr("x", function(d) { return d.x; })
+      .attr("y", function(d) { return d.y; });
+    });
+
+    simulation.restart();
+  }
+
+  render() {
     let { width, height } = this.state;
 
     setTimeout(() => {
-      // render labels with D3
-      let g = d3.select('.labels');
-      let projection = this.projection();
-      g.selectAll('text')
-      .data(this.props.markers)
-      .enter()
-      .append('text')
-      .attr('dy', '.35em')
-      .attr('transform', function(d) {
-        let { lat, lng } = d.location;
-        let points = projection([lng, lat]);
-        return `translate(${points[0]}, ${points[1]})`;
-      })
-      .text(function(d) {
-        let { city } = d.location;
-        return city.split(',')[0];
-      })
-      .attr('class', function(d) { return `label label-${d.location.city}`; });
+      this.renderNodes();
+      this.renderLabels();
     }, 0);
 
     return (
@@ -101,30 +170,6 @@ class WorldMap extends Component {
           }
         </g>
         <g className="markers">
-          {
-            this.props.markers.map((m, i) => {
-              let { lat, lng } = m.location;
-
-              let projection = this.projection();
-              let points = projection([lng, lat]);
-
-              return (
-                <circle
-                  key={`marker-${m.location.city}`}
-                  cx={ points[0] }
-                  cy={ points[1] }
-                  r={ calculateRadius(m.events.length) }
-                  fill="#E91E63"
-                  className={`marker marker-${m.location.city}`}
-                  onClick={ () => this.handleMarkerClick(i) }
-                >
-                  <text>
-                    {m.location.city}
-                  </text>
-                </circle>
-              );
-            })
-          }
         </g>
         <g className="labels">
         </g>
